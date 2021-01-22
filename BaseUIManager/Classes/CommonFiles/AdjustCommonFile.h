f85aef514d8bd7dd5212f1fc2dd6ce62efda41be
Sumansoul <13162378587@163.com>
2021-01-22 10:08:03 +0800

    1.4.3

--- BaseUIManager/Classes/CommonFiles/AdjustCommonFile.h
+++ BaseUIManager/Classes/CommonFiles/AdjustCommonFile.h
@@ -16,32 +16,32 @@
 #define  yScreenHeight  [UIScreen mainScreen].bounds.size.height
 
 //navigationBar height
-#define kNavigationBarHeight ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax) ? 68 : 44)
+#define kNavigationBarHeight (kIsIPHONE ? ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax || kIphone12Mini || kIphone12Pro || kIphone12ProMax) ? 68 : 44): 50)
 
 //statusBar height
 #define kStatusBarHeight 20
 
 //navigationSafetySpaceHeight
-#define kNavigationTopSpace ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax) ? 24 : 0)
+#define kNavigationTopSpace ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax || kIphone12Mini || kIphone12Pro || kIphone12ProMax) ? 24 : 0)
 
 //bottomSafetySpaceHeight
-#define kIphoneXBottomSpace ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax) ? 34 : 0)
+#define kIphoneXBottomSpace ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax || kIphone12Mini || kIphone12Pro || kIphone12ProMax) ? 34 : 0)
 
 //StatusBarHeight + navigationBarHeight
-#define kNavigationHeight ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax) ? 88 : 64)
+#define kNavigationHeight (kIsIPHONE ? ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax || kIphone12Mini || kIphone12Pro || kIphone12ProMax) ? 88 : 64) : 70)
 
 //tabbar高度
-#define kTabbarHeight ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax) ? 83 : 49)
+#define kTabbarHeight ((kIPhoneX || kIPhoneXR || kIPhoneXS || kIPhoneXSMax || kIphone12Mini || kIphone12Pro || kIphone12ProMax) ? 83 : 49)
 
-//是否是iphone
+//is iphone?
 #define kIsIPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 
-//是否是ipad
+//is ipad?
 #define kIsIpad (UI_USER_INTERFACE_IDIOM()== UIUserInterfaceIdiomPad)
 
 //adjustPercentSize depends on whatever your device is ,（your device physical height / iphone 6's physical height）
-#define kPercentageHeight(height) (yScreenHeight *(height)/667)
-#define kPercentageWidth(width) (yScreenWidth / 375 * width)
+#define kPercentageHeight(height) (kIsIPHONE ? (yScreenHeight *height/667): (yScreenHeight *height/1024))
+#define kPercentageWidth(width) (kIsIPHONE ? (yScreenWidth / 375 * width): (yScreenWidth / 768 * width))
 
 //the current device is Retina or not device;
 #define kIsRetina ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
@@ -73,8 +73,14 @@
 //if it was iphone xs
 #define kIPhoneXS ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !kisPad : NO)
 
-//if it was iphone xs max
+//判断iPhoneXs Max
 #define kIPhoneXSMax ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !kisPad : NO)
+//判断iPHone12 mini
+#define kIphone12Mini ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1080, 2340), [[UIScreen mainScreen] currentMode].size) && !kisPad : NO)
+//判断iPHone12 和 iPHone12 Pro
+#define kIphone12Pro ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1170, 2532), [[UIScreen mainScreen] currentMode].size) && !kisPad : NO)
+//判断iPHone12 ProMax
+#define kIphone12ProMax ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1284, 2778), [[UIScreen mainScreen] currentMode].size) && !kisPad : NO)
  
 //the current systemVersion is iOS 10.0 or not
 #define IOS10_OR_LATER ([[[[[UIDevice currentDevice] systemVersion] substringToIndex:1] stringByAppendingString:@"0"] intValue] == 10)
