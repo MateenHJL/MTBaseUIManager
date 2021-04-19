36b6902d9f124dccbdc6659173454c649ef17a48
Sumansoul <13162378587@163.com>
2021-04-19 14:34:19 +0800

    1.4.8

--- BaseUIManager/Classes/Category/UIFont+AdjustFont.m
+++ BaseUIManager/Classes/Category/UIFont+AdjustFont.m
@@ -15,10 +15,26 @@
 {
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
     {
+        if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait)
+        {
+            return size * [[UIScreen mainScreen]bounds].size.width / 375.0;
+        }
+        else if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationLandscapeLeft)
+        {
+            return size * [[UIScreen mainScreen]bounds].size.height / 667.0;
+        }
         return size * [[UIScreen mainScreen]bounds].size.width / 375.0;
     }
     else
     {
+        if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationPortrait)
+        {
+            return size * [UIScreen mainScreen].bounds.size.width / 768.0;
+        }
+        else if ([UIApplication sharedApplication].statusBarOrientation == UIInterfaceOrientationLandscapeLeft)
+        {
+            return size * [[UIScreen mainScreen]bounds].size.height / 1024.0;
+        }
         return size * [UIScreen mainScreen].bounds.size.width / 768.0;
     }
 }
